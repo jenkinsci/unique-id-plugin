0d212d1a2963f80a6f6d67bf22f1ebc60a57115e
Stephen Connolly <stephen.alan.connolly@gmail.com>
2015-07-14 13:02:01 +0100

    Expose the filename as a public non-inlinable constant
    - Needs to come via a static method in case we ever need to change it so that dependencies using it do not have to be recompiled

--- src/main/java/org/jenkinsci/plugins/uniqueid/implv2/PersistenceRootIdStore.java
+++ src/main/java/org/jenkinsci/plugins/uniqueid/implv2/PersistenceRootIdStore.java
@@ -28,16 +28,13 @@ public class PersistenceRootIdStore extends IdStore<PersistenceRoot> {
     /** Our Logger. */
     private final static Logger LOGGER = Logger.getLogger(PersistenceRootIdStore.class.getName());
 
-    /** The name of the file in which we store the unique ID. */
-    private final static String ID_FILE = "unique-id.txt";
-
     public PersistenceRootIdStore() {
         super(PersistenceRoot.class);
     }
 
     @Override
     public void make(PersistenceRoot object) {
-        File f = new File(object.getRootDir(), ID_FILE);
+        File f = new File(object.getRootDir(), IdStore.fileName());
         if (!f.exists()) {
             File tmp = null;
             try {
@@ -59,7 +56,7 @@ public class PersistenceRootIdStore extends IdStore<PersistenceRoot> {
 
     @Override
     public String get(PersistenceRoot object) {
-        File f = new File(object.getRootDir(), ID_FILE);
+        File f = new File(object.getRootDir(), IdStore.fileName());
         if (f.exists() && f.canRead()) {
             try {
                 String str = FileUtils.readFileToString(f, StandardCharsets.UTF_8);
@@ -79,7 +76,7 @@ public class PersistenceRootIdStore extends IdStore<PersistenceRoot> {
 
     @Restricted(NoExternalUse.class)
     public static void create(PersistenceRoot object, String uniqueId) throws IOException {
-        File f = new File(object.getRootDir(), ID_FILE);
+        File f = new File(object.getRootDir(), IdStore.fileName());
         if (!f.exists()) {
             LOGGER.log(Level.FINE, "Creating file ({1}) to store ID for ({0}) whose RootDir is ({2}).", new Object[] {object.toString(), f, object.getRootDir()});
             // no need to migrate if its there to begin with!
